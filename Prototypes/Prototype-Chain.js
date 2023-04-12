861eb74880f2ec24af9159e9f2b21ace828f2923
Ahmad Khan <ahmad.d.khan40@gmail.com>
2023-04-11 22:33:53 -0400

    aaa

--- Prototypes/Prototype-Chain.js
+++ Prototypes/Prototype-Chain.js
@@ -6,9 +6,32 @@ function Person(firstName, lastName, favoriteColor, favoriteNumber, favoriteFood
     this.favoriteFoods = favoriteFoods
 }
 Person.prototype.fullName = function(){
-    return `Full Name: ${this.firstName} ${this.lastName}`
+    return `Full Name: ${this.firstName} ${this.lastName}`;
 }
 
-    let jerry = new Person("Jerry", "Seinfield", "Blue", "2", ["Airplane food", "Peanuts"])
+Person.prototype.toString = function(){
+    return `Full Name: ${this.firstName} ${this.lastName}, Favorite Color: ${this.favoriteColor}, Favorite Number: ${this.favoriteNumber}`
+}
+
+Person.prototype.family = [];
+
+Person.prototype.addToFamily = function (newFamily) {
+    if(newFamily instanceof Person && !this.family.includes(newFamily))
+    this.family.push(newFamily)
+    return  this.family.length
+}
+    
+
+
+    let jerry = new Person("Jerry", "Seinfeld", "Green", "2", ["Airplane food", "Peanuts"])
     console.log(jerry.fullName())
-    console.log(jerry)
+    console.log(jerry.toString())
+
+    let chuck = new Person("Chuck", "Norris", "Red", "0", ["Wood Chips", "Beer"])
+
+    let walter = new Person("Walter", "White", "Blue", "9,000,000", ["Blue Candy", "Eggs", "Bacon"])
+
+    jerry.addToFamily(jerry)
+    chuck.addToFamily(chuck)
+    walter.addToFamily(walter)
+    console.log(Person.prototype.family)
