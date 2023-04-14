08b5032827658f94f545bc5747538fafa5db5d5e
Ahmad Khan <ahmad.d.khan40@gmail.com>
2023-04-14 08:25:04 -0400

    d

---
+++ This-activity/this.js
@@ -0,0 +1,46 @@
+let photoSlideShow = {
+
+    photoList: [`image1`, `image2`, `image3`, `image4`],
+
+    currentPhotoIndex: 1,
+
+    nextPhoto: function () {
+        if(this.currentPhotoIndex < (this.photoList.length -1)){
+            return this.currentPhotoIndex++,
+             console.log("Current Photo:" + `${this.photoList[this.currentPhotoIndex]}`);
+          } else{ 
+            console.log("End of Slideshow.")
+    }    
+    },
+
+    prevPhoto: function () {
+        if(this.currentPhotoIndex > 0){
+            this.currentPhotoIndex--;
+        console.log("Current Photo:" + `${this.photoList[this.currentPhotoIndex]}`);
+        } else{ 
+        console.log("End of Slideshow.")
+        }
+    },
+
+getCurrentPhoto: function () {
+    return this.photoList[this.currentPhotoIndex]
+}
+}
+// Next Photo
+    console.log(photoSlideShow.currentPhotoIndex);
+    photoSlideShow.nextPhoto();
+    console.log(photoSlideShow.currentPhotoIndex);
+    photoSlideShow.nextPhoto();
+
+// // prevPhoto
+    console.log(photoSlideShow.currentPhotoIndex);
+    photoSlideShow.prevPhoto();
+    console.log(photoSlideShow.currentPhotoIndex);
+    photoSlideShow.prevPhoto();
+
+// // getCurrentPhoto
+
+    console.log(photoSlideShow.getCurrentPhoto());
+    photoSlideShow.nextPhoto();
+    console.log(photoSlideShow.getCurrentPhoto());
+    photoSlideShow.nextPhoto();
